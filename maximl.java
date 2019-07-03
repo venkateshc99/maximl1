2a513e5f22a3ed0ccb5cf392b136749d47e7bef6
venkateshc99 <52483355+venkateshc99@users.noreply.github.com>
2019-07-03 11:56:52 +0530

    Create maximl.java

---
+++ maximl.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class maximl 
+{
+    public static void main(String []args)
+    {
+     Scanner s=new Scanner(System.in);
+     String a=s.nextLine();
+     TreeSet<Character>b=new TreeSet<>();
+     char[]c=a.toCharArray();
+     for(int i=0;i<c.length;i++)
+     b.add(c[i]);
+        System.out.println(b.size());
+     }
+}
